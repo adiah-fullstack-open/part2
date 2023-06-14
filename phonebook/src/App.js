b87852f13d0e30d861d8f50ce327651fcdd83a7f
nathanieladiah <nathanieladiah@gmail.com>
2023-06-14 13:28:45 -0400

    Get initial data from server

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import AddForm from "./components/AddForm";
 import Filter from "./components/Filter";
 import Phonebook from "./components/Phonebook";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "472-3716", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-63-234356", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
