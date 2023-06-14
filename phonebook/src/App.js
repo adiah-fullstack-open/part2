9158624f35c71896996e72e728ac5a4cd8b3be1a
nathanieladiah <nathanieladiah@gmail.com>
2023-06-14 10:42:49 -0400

    Allow adding names to phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,50 @@
-function App() {
-  return <h1>app</h1>;
-}
+import React, { useState } from "react";
+import Entry from "./components/Entry";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "472-3716", id: 1 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName, id: persons.length + 1 }]);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person) => (
+            <Entry person={person} key={person.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default App;
