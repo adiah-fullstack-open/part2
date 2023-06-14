c4f7b68153939da6e57dd647628f61f661f9b6d7
nathanieladiah <nathanieladiah@gmail.com>
2023-06-14 11:30:48 -0400

    Prevent adding duplicate names

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -13,8 +13,23 @@ const App = () => {
     setNewName(event.target.value);
   };
 
+  // const checkDuplicates = (name) => {
+  //   const duplicates = persons.filter((person) => person.name === name);
+
+  //   return duplicates.length > 0;
+  // };
+
+  const checkDuplicates = (name) =>
+    persons.filter((person) => person.name === name).length > 0;
+
   const addPerson = (event) => {
     event.preventDefault();
+
+    if (checkDuplicates(newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+
     setPersons([...persons, { name: newName, id: persons.length + 1 }]);
   };
 
